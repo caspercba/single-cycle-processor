a1e0ad38d452576a5e4911019a3c14eb1851f7b2
Gaspar de Elias <caspercba@gmail.com>
2022-10-30 11:21:33 -0300

    alu add now works, except when result is -0 different than 0

--- src/alu/alu.vhd
+++ src/alu/alu.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 --use std.textio.all;
 library work;
 use work.alu_helper.ALL;
@@ -19,26 +20,33 @@ end alu;
 
 architecture behavioural of alu is
 
-		signal temp: std_logic_vector(64 downto 0);
+		signal temp: std_logic_vector(63 downto 0);
 	--alias D_address_sign is a(20);
 	--alias CB_address_sign is a(23);
 begin
 	process(a,b,ALUControl)
 	begin
+		report "Operation: " & to_string(ALUControl) & " params: " & to_hex_string(a) & ", " & to_hex_string(b);
 		if(ALUControl=CTRL_AND) then
-			result<=a and b;
+			result<=(a and b);
 		elsif(ALUControl=CTRL_OR) then
 			result<=a or b;
 		elsif(ALUControl=CTRL_ADD) then
-			result<=a xor b;
+			result<=std_logic_vector(signed(a) + signed(b));
 		elsif(ALUControl=CTRL_SUB) then
 			result<=a xor not b;
 		elsif(ALUControl=CTRL_PASS) then
-			result<=b;
+			result<=b(63) & b;
 		elsif(ALUControl=CTRL_NOR) then
 			result<=a nor b;
 		else
 			result<=(others=>'0');
+		end if;
+		
+		if result = (( result'reverse_range) => '0') then
+			zero<='1';
+		else
+			zero <='0';
 		end if;
 end process;
 end behavioural;
--- src/alu/alu_tb.vhd
+++ src/alu/alu_tb.vhd
@@ -11,10 +11,18 @@ entity alu_tb is
 end alu_tb;
 
 architecture test of alu_tb is
-	
-	constant num1: integer :=-50;
-	constant num2: integer :=-50;
-	constant num_max: std_logic_vector(63 downto 0) := x"FFFFFFFFFFFFFFFF";  
+
+
+	subtype param is std_logic_vector(63 downto 0);
+
+	constant num_zero: param:= x"0000000000000000";
+	constant num_one : param:= x"0000000000000001";
+	constant num_two : param:= x"0000000000000002";
+	constant num_three : param:= x"0000000000000003";
+	constant num_four : param:= x"0000000000000004";
+	constant neg_one : param:= x"FFFFFFFFFFFFFFFE";
+	constant num_max : param:= x"FFFFFFFFFFFFFFFF";
+	constant num_100 : param:= x"0000000000000000";
 
 	component alu is
 		port(
@@ -25,9 +33,25 @@ architecture test of alu_tb is
 	);
 	end component alu;
 	
-	signal a,b,result: std_ulogic_vector(63 downto 0);
+	signal a,b,result: std_logic_vector(63 downto 0);
 	signal ALUControl: std_logic_vector(3 downto 0);
 	signal zero	 : std_logic;
+	
+	type test_vector is record
+		ALUControl	: std_logic_vector(3 downto 0);
+		a,b,result	: param;
+		zero		: std_logic;
+	end record;
+
+	type test_data is array (natural range<>) of test_vector;
+
+	constant tests: test_data := (
+	(CTRL_AND, num_zero, num_zero, num_zero,'1'),
+	(CTRL_AND, num_one, num_one, num_one, '0'),
+	(CTRL_AND, num_one, num_three, num_one, '0'), 
+	(CTRL_ADD, num_one, num_one, num_two, '0'),
+	(CTRL_ADD, num_one, neg_one, num_zero, '1')	
+);
 
 begin
 	UUT: entity work.alu
@@ -35,14 +59,18 @@ begin
 		stimulus : process
 			variable expected: std_logic_vector(63 downto 0);
 		begin
-			wait for 1 ns;
-			a<=std_ulogic_vector(to_signed(num1, a'length));
-			b<=std_ulogic_vector(to_signed(num2, b'length));
-			ALUControl<=CTRL_ADD;
-			expected := (others => '0');
-			wait for 1 ns;
-			assert(result = expected);
-			wait for 1 ns;
+			wait for 10 ns;
+			for i in tests'range loop
+				report "Testing " & integer'image(i);
+				a<=tests(i).a;
+				b<=tests(i).b;
+				ALUControl<=tests(i).ALUControl;
+				wait for 100 ns;
+				assert(result = tests(i).result);
+				report "result: " & to_hex_string(result)
+				severity error;
+			--assert(zero = tests(i).zero);
+			end loop;
 			stop;
 		end process stimulus;
 end test;
