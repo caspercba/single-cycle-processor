15bc47c40ea26b973d783f8f99f55322d1504075
Gaspar de Elias <gdeelias@paypal.com>
2022-11-12 12:38:53 -0300

    MOst of the implementation compiling, but having some issues with analysis stage for maindec

--- src/maindec/maindec.vhd
+++ src/maindec/maindec.vhd
@@ -5,6 +5,7 @@ use ieee.numeric_std.all;
 
 library work;
 use work.logger.log;
+use work.operations.all;
 
 entity maindec is
 	port(
@@ -18,52 +19,94 @@ architecture behavioural of maindec is
 	-- signals or constants here
 begin
 
+	with Op select
+	RegToLoc <= 	'0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_LDUR,
+		    	'1' when OP_STUR|OP_CBZ_COMPAT,
+		    	'0' when others;
+	
+	with Op select
+	ALUSrc <= '0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_CBZ_COMPAT,
+		    '1' when OP_LDUR|OP_STUR,
+		    '0' when others;
 
-	case Op is
-		when OP_ADD|OP_SUB|OP_AND|OP_ORR	=> 
-				RegToLoc	<=	'0';
-				ALUSrc		<=	'0';
-				MemToReg	<=	'0';
-				RegWrite	<=	'1';
-				MemRead		<=	'0';
-				MemWrite	<=	'0';
-				Branch		<=	'0';
-				ALUOp		<=	"10";
-		when OP_LDUR	=>
-				RegToLoc	<=	'0';
-				ALUSrc		<=	'1';
-				MemToReg	<=	'1';
-				RegWrite	<=	'1';
-				MemRead		<=	'1';
-				MemWrite	<=	'0';
-				Branch		<=	'0';
-				ALUOp		<=	"00";
-		when OP_STUR	=>
-				RegToLoc	<=	'1';
-				ALUSrc		<=	'1';
-				MemToReg	<=	'0';
-				RegWrite	<=	'0';
-				MemRead		<=	'0';
-				MemWrite	<=	'1';
-				Branch		<=	'0';
-				ALUOp		<=	"00";
-		when OP_CBZ	=>
-				RegToLoc	<=	'1';
-				ALUSrc		<=	'0';
-				MemToReg	<=	'0';
-				RegWrite	<=	'0';
-				MemRead		<=	'0';
-				MemWrite	<=	'0';
-				Branch		<=	'1';
-				ALUOp		<=	"01";
-		when others	=>
-				RegToLoc	<=	'0';
-				ALUSrc		<=	'0';
-				MemToReg	<=	'0';
-				RegWrite	<=	'0';
-				MemRead		<=	'0';
-				MemWrite	<=	'0';
-				Branch		<=	'0';
-				ALUOp		<=	"00";
-	end case;
+	with Op select
+	MemToReg <= '0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_STUR|OP_CBZ_COMPAT,
+		    '1' when OP_LDUR,
+		    '0' when others;
+
+	with Op select
+	RegWrite <= '1' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_LDUR,
+		    '0' when OP_STUR|OP_CBZ_COMPAT,
+		    '0' when others;
+
+	with Op select
+	MemRead <= '0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_STUR|OP_CBZ_COMPAT,
+		   '1' when OP_LDUR,
+		   '0' when others;
+
+	with Op select
+	MemWrite <= 	'0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_LDUR|OP_CBZ_COMPAT,
+		   	'1' when OP_STUR,
+		   	'0' when others;
+
+	with Op select
+	Branch <= 	'0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_LDUR|OP_STUR,
+		   	'1' when OP_CBZ_COMPAT,
+		   	'0' when others;
+
+	with Op select
+	ALUOp <= 	"10" when OP_ADD|OP_SUB|OP_AND|OP_ORR,
+		   	"00" when OP_LDUR|OP_STUR,
+		   	"01" when OP_CBZ_COMPAT,
+		   	"00" when others;
+
+
+
+--	case Op is
+--		when OP_ADD|OP_SUB|OP_AND|OP_ORR	=> 
+--				RegToLoc	<=	'0';
+--				ALUSrc		<=	'0';
+--				MemToReg	<=	'0';
+--				RegWrite	<=	'1';
+--				MemRead		<=	'0';
+--				MemWrite	<=	'0';
+--				Branch		<=	'0';
+--				ALUOp		<=	"10";
+--		when OP_LDUR	=>
+--				RegToLoc	<=	'0';
+--				ALUSrc		<=	'1';
+--				MemToReg	<=	'1';
+--				RegWrite	<=	'1';
+--				MemRead		<=	'1';
+--				MemWrite	<=	'0';
+--				Branch		<=	'0';
+--				ALUOp		<=	"00";
+--		when OP_STUR	=>
+--				RegToLoc	<=	'1';
+--				ALUSrc		<=	'1';
+--				MemToReg	<=	'0';
+--				RegWrite	<=	'0';
+--				MemRead		<=	'0';
+--				MemWrite	<=	'1';
+--				Branch		<=	'0';
+--				ALUOp		<=	"00";
+--		when OP_CBZ	=>
+--				RegToLoc	<=	'1';
+--				ALUSrc		<=	'0';
+--				MemToReg	<=	'0';
+--				RegWrite	<=	'0';
+--				MemRead		<=	'0';
+--				MemWrite	<=	'0';
+--				Branch		<=	'1';
+--				ALUOp		<=	"01";
+--		when others	=>
+--				RegToLoc	<=	'0';
+--				ALUSrc		<=	'0';
+--				MemToReg	<=	'0';
+--				RegWrite	<=	'0';
+--				MemRead		<=	'0';
+--				MemWrite	<=	'0';
+--				Branch		<=	'0';
+--				ALUOp		<=	"00";
+--	end case;
 end architecture;	
--- src/maindec/maindec_tb.vhd
+++ src/maindec/maindec_tb.vhd
@@ -3,26 +3,30 @@ use ieee.std_logic_1164.all;
 
 library work;
 use work.operations.all;
+use work.logger.log;
+
+entity maindec_tb is
+end maindec_tb;
 
 architecture test of maindec_tb is
 
-	subtype op is std_logic_vector(10 downto 0);
+	subtype operation is std_logic_vector(10 downto 0);
 
 
 	component maindec is
 		port(
-			Op	: in op;
+			Op	: in operation;
 			RegToLoc,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch: out std_logic;
 			ALUOp	: out std_logic_vector(1 downto 0)
 		);
 	end component maindec;
 
-			signal Op	: op;
+			signal Op	: operation;
 			signal RegToLoc,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch: std_logic;
-			signal ALUOp	: std_logic_vector(1 downto 0)
+			signal ALUOp	: std_logic_vector(1 downto 0);
 	
 	type test_vector is record
-		Op	:	op;
+		Op	:	operation;
 		RegToLoc,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch: std_logic;
 		ALUOp	:	std_logic_vector(1 downto 0);
 	end record test_vector;
@@ -30,43 +34,43 @@ architecture test of maindec_tb is
 	type test_data is array (natural range <>) of test_vector;
 
 	constant tests: test_data := (
-	(OP_ADD, 0, 0, 0, 1, 0, 0, 0, "10"),
-	(OP_SUB, 0, 0, 0, 1, 0, 0, 0, "10"),
-	(OP_AND, 0, 0, 0, 1, 0, 0, 0, "10"),
-	(OP_ORR, 0, 0, 0, 1, 0, 0, 0, "10"),
-	(OP_LDUR, 0, 1, 1, 1, 1, 0, 0, "00"),
-	(OP_STUR, 1, 1, 0, 0, 0, 1, 0, "00"),
-	(OP_CBZ, 1, 0, 0, 1, 0, 0, 1, "01"),
-	("11111111111", 0, 0, 0, 0, 0, 0, 0, "00")
+	(OP_ADD, '0', '0', '0', '1', '0', '0', '0', "10"),
+	(OP_SUB, '0', '0', '0', '1', '0', '0', '0', "10"),
+	(OP_AND, '0', '0', '0', '1', '0', '0', '0', "10"),
+	(OP_ORR, '0', '0', '0', '1', '0', '0', '0', "10"),
+	(OP_LDUR, '0', '1', '1', '1', '1', '0', '0', "00"),
+	(OP_STUR, '1', '1', '0', '0', '0', '1', '0', "00"),
+	(OP_CBZ, '1', '0', '0', '1', '0', '0', '1', "01"),
+	("11111111111", '0', '0', '0', '0', '0', '0', '0', "00")
 );
 
 
 begin
 	UUT: entity work.maindec
 	port map(	
-		Op	=>Op;
-		RegToLoc=>RegToLoc;
-		ALUSrc	=> ALUScr;
-		MemtoReg=> MemtoReg;
-		RegWrite=> RegWrite;
-		MemRead	=> MemRead;
-		MemWrite=> MemWrite;
+		Op	=>Op,
+		RegToLoc=>RegToLoc,
+		ALUSrc	=> ALUSrc,
+		MemtoReg=> MemtoReg,
+		RegWrite=> RegWrite,
+		MemRead	=> MemRead,
+		MemWrite=> MemWrite,
 		Branch	=> Branch
 	);
 	
 	stimulus: process
 	begin
 		for i in tests'range loop
-			log("maindec testcase #" & to_string(i) & " of " & to_string(tests'length)
+			log("maindec testcase #" & to_string(i) & " of " & to_string(tests'length));
 			Op<=tests(i).Op;
-			assert RegToLoc=tests(i).RegToLoc report "Expected: " & to_hex_string(tests(i).RegToLoc) & " Got: " & to_hex_string(RegToLoc) severity error;
-			assert ALUSrc=tests(i).ALUSrc report "Expected: " & to_hex_string(tests(i).ALUSrc) & " Got: " & to_hex_string(ALUSrc) severity error;
-			assert MemtoReg=tests(i).MemtoReg report "Expected: " & to_hex_string(tests(i).MemtoReg) & " Got: " & to_hex_string(MemtoReg) severity error;
-			assert RegWrite=tests(i).RegWrite report "Expected: " & to_hex_string(tests(i).MemtoReg) & " Got: " & to_hex_string(RegWrite) severity error;
-			assert MemRead=tests(i).MemRead report "Expected: " & to_hex_string(tests(i).MemRead) & " Got: " & to_hex_string(MemRead) severity error;
-			assert MemWrite=tests(i).MemWrite report "Expected: " & to_hex_string(tests(i).MemWrite) & " Got: " & to_hex_string(MemWrite) severity error;
-			assert Branch=tests(i).Branch report "Expected: " & to_hex_string(tests(i).Branch) & " Got: " & to_hex_string(Branch) severity error;
+			assert RegToLoc=tests(i).RegToLoc report "Expected: " & to_string(tests(i).RegToLoc) & " Got: " & to_string(RegToLoc) severity error;
+			assert ALUSrc=tests(i).ALUSrc report "Expected: " & to_string(tests(i).ALUSrc) & " Got: " & to_string(ALUSrc) severity error;
+			assert MemtoReg=tests(i).MemtoReg report "Expected: " & to_string(tests(i).MemtoReg) & " Got: " & to_string(MemtoReg) severity error;
+			assert RegWrite=tests(i).RegWrite report "Expected: " & to_string(tests(i).MemtoReg) & " Got: " & to_string(RegWrite) severity error;
+			assert MemRead=tests(i).MemRead report "Expected: " & to_string(tests(i).MemRead) & " Got: " & to_string(MemRead) severity error;
+			assert MemWrite=tests(i).MemWrite report "Expected: " & to_string(tests(i).MemWrite) & " Got: " & to_string(MemWrite) severity error;
+			assert Branch=tests(i).Branch report "Expected: " & to_string(tests(i).Branch) & " Got: " & to_string(Branch) severity error;
 		end loop;
 	end process stimulus;
-end architecture;
+end test;
 
