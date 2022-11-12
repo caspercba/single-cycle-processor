450259871058c6f707c55c5ceb9f9e210403d166
Gaspar de Elias <gdeelias@paypal.com>
2022-11-12 14:15:02 -0300

    maindec now working with testbenches.

--- src/maindec/maindec.vhd
+++ src/maindec/maindec.vhd
@@ -9,7 +9,7 @@ use work.operations.all;
 
 entity maindec is
 	port(
-	Op	: in std_logic_vector(10 downto 0);
+	op	: in std_logic_vector(10 downto 0);
 	RegToLoc,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch: out std_logic;
 	ALUOp	: out std_logic_vector(1 downto 0)
 );
@@ -19,42 +19,42 @@ architecture behavioural of maindec is
 	-- signals or constants here
 begin
 
-	with Op select
+	with op select
 	RegToLoc <= 	'0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_LDUR,
 		    	'1' when OP_STUR|OP_CBZ_COMPAT,
 		    	'0' when others;
 	
-	with Op select
+	with op select
 	ALUSrc <= '0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_CBZ_COMPAT,
 		    '1' when OP_LDUR|OP_STUR,
 		    '0' when others;
 
-	with Op select
+	with op select
 	MemToReg <= '0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_STUR|OP_CBZ_COMPAT,
 		    '1' when OP_LDUR,
 		    '0' when others;
 
-	with Op select
+	with op select
 	RegWrite <= '1' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_LDUR,
 		    '0' when OP_STUR|OP_CBZ_COMPAT,
 		    '0' when others;
 
-	with Op select
+	with op select
 	MemRead <= '0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_STUR|OP_CBZ_COMPAT,
 		   '1' when OP_LDUR,
 		   '0' when others;
 
-	with Op select
+	with op select
 	MemWrite <= 	'0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_LDUR|OP_CBZ_COMPAT,
 		   	'1' when OP_STUR,
 		   	'0' when others;
 
-	with Op select
+	with op select
 	Branch <= 	'0' when OP_ADD|OP_SUB|OP_AND|OP_ORR|OP_LDUR|OP_STUR,
 		   	'1' when OP_CBZ_COMPAT,
 		   	'0' when others;
 
-	with Op select
+	with op select
 	ALUOp <= 	"10" when OP_ADD|OP_SUB|OP_AND|OP_ORR,
 		   	"00" when OP_LDUR|OP_STUR,
 		   	"01" when OP_CBZ_COMPAT,
--- src/maindec/maindec_tb.vhd
+++ src/maindec/maindec_tb.vhd
@@ -4,31 +4,32 @@ use ieee.std_logic_1164.all;
 library work;
 use work.operations.all;
 use work.logger.log;
+use std.env.stop;
 
 entity maindec_tb is
 end maindec_tb;
 
 architecture test of maindec_tb is
 
-	subtype operation is std_logic_vector(10 downto 0);
-
+	subtype op_type is std_logic_vector(R_OP_SIZE-1 downto 0);
+	subtype alu_op_type is std_logic_vector(1 downto 0);
 
 	component maindec is
 		port(
-			Op	: in operation;
+			op	: in op_type;
 			RegToLoc,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch: out std_logic;
-			ALUOp	: out std_logic_vector(1 downto 0)
+			ALUOp	: out alu_op_type 
 		);
 	end component maindec;
 
-			signal Op	: operation;
-			signal RegToLoc,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch: std_logic;
-			signal ALUOp	: std_logic_vector(1 downto 0);
+	signal op	: op_type; 
+	signal RegToLoc, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch : std_logic := '0';
+	signal ALUOp	: alu_op_type; 
 	
 	type test_vector is record
-		Op	:	operation;
+		op	:	op_type;
 		RegToLoc,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch: std_logic;
-		ALUOp	:	std_logic_vector(1 downto 0);
+		ALUOp	:	alu_op_type;
 	end record test_vector;
 
 	type test_data is array (natural range <>) of test_vector;
@@ -40,7 +41,7 @@ architecture test of maindec_tb is
 	(OP_ORR, '0', '0', '0', '1', '0', '0', '0', "10"),
 	(OP_LDUR, '0', '1', '1', '1', '1', '0', '0', "00"),
 	(OP_STUR, '1', '1', '0', '0', '0', '1', '0', "00"),
-	(OP_CBZ, '1', '0', '0', '1', '0', '0', '1', "01"),
+	(OP_CBZ_COMPAT, '1', '0', '0', '0', '0', '0', '1', "01"),
 	("11111111111", '0', '0', '0', '0', '0', '0', '0', "00")
 );
 
@@ -48,29 +49,33 @@ architecture test of maindec_tb is
 begin
 	UUT: entity work.maindec
 	port map(	
-		Op	=>Op,
+		op	=>op,
 		RegToLoc=>RegToLoc,
 		ALUSrc	=> ALUSrc,
 		MemtoReg=> MemtoReg,
 		RegWrite=> RegWrite,
 		MemRead	=> MemRead,
 		MemWrite=> MemWrite,
-		Branch	=> Branch
+		Branch	=> Branch,
+		ALUOp	=> ALUOp
 	);
 	
 	stimulus: process
 	begin
 		for i in tests'range loop
-			log("maindec testcase #" & to_string(i) & " of " & to_string(tests'length));
-			Op<=tests(i).Op;
-			assert RegToLoc=tests(i).RegToLoc report "Expected: " & to_string(tests(i).RegToLoc) & " Got: " & to_string(RegToLoc) severity error;
-			assert ALUSrc=tests(i).ALUSrc report "Expected: " & to_string(tests(i).ALUSrc) & " Got: " & to_string(ALUSrc) severity error;
-			assert MemtoReg=tests(i).MemtoReg report "Expected: " & to_string(tests(i).MemtoReg) & " Got: " & to_string(MemtoReg) severity error;
-			assert RegWrite=tests(i).RegWrite report "Expected: " & to_string(tests(i).MemtoReg) & " Got: " & to_string(RegWrite) severity error;
-			assert MemRead=tests(i).MemRead report "Expected: " & to_string(tests(i).MemRead) & " Got: " & to_string(MemRead) severity error;
-			assert MemWrite=tests(i).MemWrite report "Expected: " & to_string(tests(i).MemWrite) & " Got: " & to_string(MemWrite) severity error;
-			assert Branch=tests(i).Branch report "Expected: " & to_string(tests(i).Branch) & " Got: " & to_string(Branch) severity error;
+			log("maindec testcase #" & to_string(i+1) & " of " & to_string(tests'length));
+			op<=tests(i).op;
+			wait for 1 ps;
+			assert RegToLoc=tests(i).RegToLoc report "RegToLoc Expected: " & to_string(tests(i).RegToLoc) & " Got: " & to_string(RegToLoc) severity error;
+			assert ALUSrc=tests(i).ALUSrc report "ALUSrc Expected: " & to_string(tests(i).ALUSrc) & " Got: " & to_string(ALUSrc) severity error;
+			assert MemtoReg=tests(i).MemtoReg report "MemtoReg Expected: " & to_string(tests(i).MemtoReg) & " Got: " & to_string(MemtoReg) severity error;
+			assert RegWrite=tests(i).RegWrite report "RegWrite Expected: " & to_string(tests(i).MemtoReg) & " Got: " & to_string(RegWrite) severity error;
+			assert MemRead=tests(i).MemRead report "MemRead Expected: " & to_string(tests(i).MemRead) & " Got: " & to_string(MemRead) severity error;
+			assert MemWrite=tests(i).MemWrite report "MemWrite Expected: " & to_string(tests(i).MemWrite) & " Got: " & to_string(MemWrite) severity error;
+			assert Branch=tests(i).Branch report "Branch Expected: " & to_string(tests(i).Branch) & " Got: " & to_string(Branch) severity error;
+			assert ALUOp=tests(i).ALUOp report "ALUOp Expected: " & to_hex_string(tests(i).ALUOp) & " Got: " & to_hex_string(ALUOp) severity error;
 		end loop;
+		stop;
 	end process stimulus;
 end test;
 
